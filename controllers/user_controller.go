f5fbb0968100f9dd253e689f9cb8797c394a3a29
snowitty <snowitty@163.com>
2023-07-08 02:36:06 +0800

    add UpdateUser

--- controllers/user_controller.go
+++ controllers/user_controller.go
@@ -30,6 +30,33 @@ func GetUser(c *fiber.Ctx) error {
 	return c.JSON(user)
 }
 
+func UpdateUser(c *fiber.Ctx) error {
+	id := c.Params("id")
+
+	user := new(models.User)
+	if err := utils.DB.First(&user, id).Error; err != nil {
+		return err
+	}
+
+	newUser := new(models.User)
+	if err := c.BodyParser(newUser); err != nil {
+		return err
+	}
+
+	user.Username = newUser.Username
+	user.Password = newUser.Password
+	user.Email = newUser.Email
+	user.NickName = newUser.NickName
+	user.Avatar = newUser.Avatar
+
+	if err := utils.DB.Save(&user).Error; err != nil {
+		return err
+	}
+
+	return c.JSON(user)
+
+}
+
 func DeleteUser(c *fiber.Ctx) error {
 	id := c.Params("id")
 	user := new(models.User)
