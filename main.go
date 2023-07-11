e20f943b03856027f98176c1ea65c99d14f66777
Snowitty <snowitty@163.com>
2023-07-11 17:30:38 +0800

    Validation Middleware Fixed to  No longer global

--- main.go
+++ main.go
@@ -3,71 +3,11 @@ package main
 import (
 	"log"
 
-	"github.com/Snowitty/inkwell/models"
 	"github.com/Snowitty/inkwell/routes"
 	"github.com/Snowitty/inkwell/utils"
-	"github.com/dgrijalva/jwt-go"
 	"github.com/gofiber/fiber/v2"
 )
 
-// 全局身份验证中间件
-func authenticateUser(ctx *fiber.Ctx) error {
-	//从请求中获取JWT令牌
-	tokenString := ctx.Get("Authorization")
-
-	if tokenString == "" {
-		return ctx.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
-			"message": "Unauthorized",
-		})
-	}
-	//解析JWT令牌
-	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-		return []byte("inkwell"), nil
-	})
-
-	if err != nil {
-		return ctx.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
-			"message": "Unauthorized",
-		})
-	}
-	//处理解析错误
-	if !token.Valid {
-		return ctx.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
-			"message": "Unauthorized",
-		})
-	}
-	//上下文中保存用户信息
-	claims := token.Claims.(jwt.MapClaims)
-	userID := uint(claims["user_id"].(float64))
-	ctx.Locals("userID", userID)
-	//继续下一个中间件
-	return ctx.Next()
-}
-
-// 全局访问控制中间件
-func accessControl() fiber.Handler {
-	return func(ctx *fiber.Ctx) error {
-
-		userID, ok := ctx.Locals("userID").(uint)
-		if !ok {
-			return ctx.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
-				"message": "Unauthorized",
-			})
-		}
-
-		//访问控制逻辑
-
-		articles := []models.Articles{}
-		if err := utils.DB.Where("user_id = ?", userID).Find(&articles).Error; err != nil {
-			return err
-		}
-
-		ctx.Locals("articles", articles)
-
-		return ctx.Next()
-	}
-
-}
 func main() {
 
 	err := utils.InitDB("config/config.yaml")
@@ -76,9 +16,6 @@ func main() {
 	}
 
 	app := fiber.New()
-
-	app.Use(authenticateUser)
-	app.Use(accessControl())
 
 	// 路由设置
 	routes.Setup(app, utils.DB)
--- routes/routes.go
+++ routes/routes.go
@@ -2,10 +2,69 @@ package routes
 
 import (
 	"github.com/Snowitty/inkwell/controllers"
+	"github.com/Snowitty/inkwell/models"
+	"github.com/Snowitty/inkwell/utils"
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gofiber/fiber/v2"
 	"gorm.io/gorm"
 )
 
+func authenticateUser(ctx *fiber.Ctx) error {
+	//从请求中获取JWT令牌
+	tokenString := ctx.Get("Authorization")
+
+	if tokenString == "" {
+		return ctx.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"message": "Unauthorized",
+		})
+	}
+	//解析JWT令牌
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		return []byte("inkwell"), nil
+	})
+
+	if err != nil {
+		return ctx.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"message": "Unauthorized",
+		})
+	}
+	//处理解析错误
+	if !token.Valid {
+		return ctx.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"message": "Unauthorized",
+		})
+	}
+	//上下文中保存用户信息
+	claims := token.Claims.(jwt.MapClaims)
+	userID := uint(claims["user_id"].(float64))
+	ctx.Locals("userID", userID)
+	//继续下一个中间件
+	return ctx.Next()
+}
+
+func accessControl() fiber.Handler {
+	return func(ctx *fiber.Ctx) error {
+
+		userID, ok := ctx.Locals("userID").(uint)
+		if !ok {
+			return ctx.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+				"message": "Unauthorized",
+			})
+		}
+
+		//访问控制逻辑
+
+		articles := []models.Articles{}
+		if err := utils.DB.Where("user_id = ?", userID).Find(&articles).Error; err != nil {
+			return err
+		}
+
+		ctx.Locals("articles", articles)
+
+		return ctx.Next()
+	}
+
+}
 func Setup(app *fiber.App, db *gorm.DB) {
 	userController := controllers.NewUserController(db)
 	tagController := controllers.NewTagsController(db)
@@ -19,7 +78,7 @@ func Setup(app *fiber.App, db *gorm.DB) {
 	app.Put("/users/:id", userController.UpdateUser)
 	app.Delete("/users/:id", userController.DeleteUser)
 	app.Post("/register", userController.RegisterUser)
-	app.Post("/login", userController.Login)
+	app.Post("/login", authenticateUser, accessControl(), userController.Login)
 
 	// 标签路由
 	app.Post("/tags", tagController.CreateTag)
