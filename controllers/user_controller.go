f065e3d3e0f4a0a433e604a18fdbdcc38f8645a9
Snowitty <snowitty@163.com>
2023-07-12 16:02:53 +0800

    fix commit errpr

--- controllers/user_controller.go
+++ controllers/user_controller.go
@@ -219,19 +219,15 @@ func (c *UserController) CheckAdmin(ctx *fiber.Ctx) error {
 }
 
 func (c *UserController) CheckCurrentUser(ctx *fiber.Ctx) error {
-
-<<<<<<< HEAD
 	//获取当前登录用户
-=======
->>>>>>> f44152b4a23cf0abe9a6bb2acf83c8a6d0ab686c
 	user, err := c.GetCurrentUser(ctx)
 	if err != nil {
 		return ctx.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
 			"message": "Failed to get current user",
 		})
 	}
-<<<<<<< HEAD
-	//从路由参数中获取用户ID
+
+	//从路由参数中获取用户ID，转换为uint64
 	userIDParam := ctx.Params("id")
 	userID, err := strconv.ParseUint(userIDParam, 10, 64)
 	if err != nil {
@@ -239,22 +235,13 @@ func (c *UserController) CheckCurrentUser(ctx *fiber.Ctx) error {
 			"message": "Invalid user ID",
 		})
 	}
-	//检查当前用户
+
+	//判断用户
 	if user.ID != uint(userID) && !user.IsAdmin {
-=======
-
-	userID := ctx.Params("id")
-
-	if user.ID != userID && !user.IsAdmin {
->>>>>>> f44152b4a23cf0abe9a6bb2acf83c8a6d0ab686c
 		return ctx.Status(fiber.StatusForbidden).JSON(fiber.Map{
-			"message": "Access denied",
+			"message": "Access Denild",
 		})
 	}
 
 	return ctx.Next()
-<<<<<<< HEAD
-
-=======
->>>>>>> f44152b4a23cf0abe9a6bb2acf83c8a6d0ab686c
 }
